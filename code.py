38f0b62248cc2e845c15808e0fa1fa807de77902
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-12-27 17:43:37 +0100

    .

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
-student1=570#steps
-student2=605
-student3=536
-student4=562
-student5=600
-student6=514
+student1=170#steps
+student2=125
+student3=136
+student4=162
+student5=130
+student6=114
 
 totalsteps =student1+student2+student3+student4+student5+student6
 
@@ -11,4 +11,4 @@ averagefootsteps = totalsteps / 6
 
 
 squareofaverage = averagefootsteps ** 2
-print(squareofaverage)
+print(""squareofaverage)
