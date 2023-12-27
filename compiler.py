1e4375b6ccb6fc40cfdb9485baa972e46d142ea9
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-12-27 17:35:45 +0100

    for kcee

--- compiler.py
+++ compiler.py
@@ -1,23 +1,25 @@
+#Importing the lexer needed for lexical analysis
 from lexer import *
+#Importing the parser for parsing the tokenized source code
 from parsercomponent import *
+#Importing the interpreter used to Interprete the AST and execute the corresponding actions
 from interpreter import *
+#Imports the sys module, which provides access to some variables maintained by the Python interpreter and to functions that interact with the interpreter.
 import sys
 
-
+#Entry point for the script
 def main():
+#Retrieving the source code file from the command line arguments
+
     source = sys.argv[1]
     new_lexer = Lexer(source)
 
     tokens = new_lexer.getTokens()
-
-    # for i in tokens:
-    #     print(i)
 
     new_parser = Parser(tokens)
     asts = new_parser.runParse()
 
     new_interpreter = Interpreter(asts)
     new_interpreter.execute()
-
 
 main()
