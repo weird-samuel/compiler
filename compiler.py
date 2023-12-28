17bda7fa964cd0930c15002eb78c64e26f349b7a
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-12-28 21:37:33 +0100

    kcee again

--- compiler.py
+++ compiler.py
@@ -12,14 +12,29 @@ def main():
 #Retrieving the source code file from the command line arguments
 
     source = sys.argv[1]
+
+#Creating a new instance of the lexer class and passing the source code as an argument
+
     new_lexer = Lexer(source)
+
+ #Obtaining a list of tokens from the lexer after lexical analysis
 
     tokens = new_lexer.getTokens()
 
+#Craeting a new instance of the parser class and passing the tokens gotten from the lexer as an argument
+
     new_parser = Parser(tokens)
+
+  #Creating a new instance of the interpreter class and passing the list of ASTs as an argument
+
     asts = new_parser.runParse()
 
+#Creating a new instance of the interpreter class and passing the list of ASTs as an argument
+
     new_interpreter = Interpreter(asts)
+
+ #Interpreting and executing the actions specified by the ASTs
+
     new_interpreter.execute()
 
 main()
